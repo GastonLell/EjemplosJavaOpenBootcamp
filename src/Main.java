94253d50611264586dbd3bf7aa7ebbe65e92f5a1
GastonLell <gastonglell@gmail.com>
2022-05-27 12:00:12 -0300

    ejercicio tema 3 (funciones)

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args){
+        suma(1,2,3);
+
+        Coche miCoche = new Coche();
+
+        miCoche.incrementarPuertas();
+
+        System.out.println(miCoche.puertas);
+    }
+
+    public static void suma (int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
+
+class Coche {
+    int puertas = 4;
+
+    public void incrementarPuertas(){
+        this.puertas++;
+    }
+}

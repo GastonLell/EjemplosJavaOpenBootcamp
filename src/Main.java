84fa14fa9b8fe369132a8ddf6820b780fd143387
GastonLell <gastonglell@gmail.com>
2022-06-01 17:43:52 -0300

    cambios

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,51 @@
+
 public class Main {
 
     public static void main(String[] args){
-        suma(1,2,3);
 
-        Coche miCoche = new Coche();
+        Coche coche = new Coche(3, 180);
 
-        miCoche.incrementarPuertas();
+        System.out.println(coche.puertas);
 
-        System.out.println(miCoche.puertas);
+        coche.setAcelerar(200);
+
+        System.out.println(coche.getVelocidadActual());
+
     }
 
-    public static void suma (int a, int b, int c){
-        System.out.println(a + b + c);
-    }
 }
 
 class Coche {
     int puertas = 4;
+    int velocidadMaxima;
+    private float velocidadActual;
 
-    public void incrementarPuertas(){
-        this.puertas++;
+    //sobrecarga de constructores
+    public Coche(){
+        System.out.println("constructor sin argumentos");
+        this.puertas = 5;
+        velocidadMaxima = 90;
+    }
+
+    public Coche(int puertas, int velocidad){
+        System.out.println("constructor con argumentos");
+
+        this.puertas = puertas;
+        velocidadMaxima = velocidad;
+    }
+
+    public void setAcelerar(int value){
+        if(value > this.velocidadMaxima){
+            this.velocidadActual = this.velocidadMaxima;
+        } else {
+            this.velocidadActual += value;
+        }
+    }
+    public void setDesacelerar(){
+        this.velocidadActual -= 15;
+    }
+    public float getVelocidadActual(){
+        return this.velocidadActual;
     }
 }
+
